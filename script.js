893002222e906d51be994b4ac6c2c839fce8715e
Rokail <153230255+Rokail@users.noreply.github.com>
2024-11-17 23:51:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,25 @@
-const btnLogin = document.getElementById('btnLogin');
-const modal = document.getElementById('modal');
-const closeModal = document.getElementById('closeModal');
+const telefoneInput = document.getElementById('telefone');
+const cpfInput = document.getElementById('cpf');
+
+Inputmask("(99) 99999-9999").mask(telefoneInput);
+Inputmask("999.999.999-99").mask(cpfInput);
+
 const formCadastro = document.getElementById('formCadastro');
-
-btnLogin.addEventListener('click', () => {
-  modal.style.display = 'flex';
-});
-
-closeModal.addEventListener('click', () => {
-  modal.style.display = 'none';
-});
-
-
 formCadastro.addEventListener('submit', (e) => {
   e.preventDefault();
+
 
   const usuario = {
     nome: document.getElementById('nome').value,
     email: document.getElementById('email').value,
-    telefone: document.getElementById('telefone').value,
-    cpf: document.getElementById('cpf').value,
+    telefone: telefoneInput.value,
+    cpf: cpfInput.value,
     senha: document.getElementById('senha').value,
   };
 
   localStorage.setItem('usuario', JSON.stringify(usuario));
-  alert('Seu cadastro foi realizado!');
-  modal.style.display = 'none';
+
+  alert('Seu cadastro foi realizado com sucesso!');
+
   window.location.href = 'perfil.html';
 });
-
-
-if (window.location.pathname.includes('perfil.html')) {
-  const userProfile = document.getElementById('userProfile');
-  const usuario = JSON.parse(localStorage.getItem('usuario'));
-
-  if (usuario) {
-    userProfile.innerHTML = `
-      <p><strong>Nome:</strong> ${usuario.nome}</p>
-      <p><strong>Email:</strong> ${usuario.email}</p>
-      <p><strong>Telefone:</strong> ${usuario.telefone}</p>
-      <p><strong>CPF:</strong> ${usuario.cpf}</p>
-    `;
-  } else {
-    userProfile.innerHTML = `<p>Nenhum dado encontrado.</p>`;
-  }
-}
